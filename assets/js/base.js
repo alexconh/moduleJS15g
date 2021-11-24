2301f16cc13073c154baf8dfe94bc9e15d1662fb
alexconh <alexconh@gmail.com>
2021-11-23 21:18:35 -0600

    exercise one solved

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,1 +1,7 @@
+let value1 = parseInt(prompt("Dame un valor"))
+let value2 = parseInt(prompt("Dame un segundo valor"))
 
+if (value1!=value2)
+    value1 > value2 ? console.log(value1/value2) : console.log(value1 + value2)
+else
+    console.log(a*b)
